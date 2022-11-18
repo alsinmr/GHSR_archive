f9e07654a60bbe2ae29acce45cf93b37587dc24c
Andy <Albert.Smith-Penzel@medizin.uni-leipzig.de>
2022-11-18 21:56:06 +0100

    updates

--- setup_direct.py
+++ setup_direct.py
@@ -11,6 +11,7 @@ sys.path.append('/work/home/alsi/python')
 import pyDR
 import os
 import numpy as np
+from pyDR.misc.Averaging import appendDataObjs
 
 
 def load_helices():
@@ -35,8 +36,8 @@ When this is ported to the server, we need to change
 """
 
 #%% File locations
-proj=pyDR.Project('/work/home/alsi/GitHub/GHSR_archive/Projects/backboneHN',create=True)
-# proj=pyDR.Project()
+proj=pyDR.Project('Projects/backboneHN',create=True)
+# proj=pyDR.Project('Projects/dummy',create=True)
 mddir='/work/public/ghrelin-receptor'
 # mddir='/Volumes/My Book/GHSR'
 
@@ -48,19 +49,31 @@ trajs=[[f'WT-apo_run{k}_0.1ns_just_protein.xtc' for k in range(1,4)],
 for topo,traj1 in zip(topos,trajs):
     for traj in traj1:
         sel=pyDR.MolSelect(topo=os.path.join(mddir,topo),
-                           traj_files=os.path.join(mddir,traj),step=1,project=proj)
+                           traj_files=os.path.join(mddir,traj),
+                           step=1,project=proj,tf=None)
         
-        sel.select_bond(Nuc='N')            #Select H–N bonds
-        frames=pyDR.Frames.FrameObj(sel)    #Create frame object 
-        frames.tensor_frame(sel1=1,sel2=2)  #Define tensor frame (H–N bond)
-        _,hlx=load_helices()  #Use helices for alignment
+        resids=sel.uni.residues.resids
         
-        fr_sel=sel.molsys.select_filter(resids=np.concatenate(hlx),filter_str='name CA')
-        frames.new_frame(Type='superimpose',sel=fr_sel)  #Remove overall motion
+        for k in range(5):  #Process in 5 chunks
+            sel.select_bond(Nuc='N',resids=resids[62*k:62*(k+1)])            #Select H–N bonds
+            frames=pyDR.Frames.FrameObj(sel)    #Create frame object 
+            frames.tensor_frame(sel1=1,sel2=2)  #Define tensor frame (H–N bond)
+            _,hlx=load_helices()  #Use helices for alignment
+            
+            fr_sel=sel.molsys.select_filter(resids=np.concatenate(hlx),filter_str='name CA')
+            frames.new_frame(Type='superimpose',sel=fr_sel)  #Remove overall motion
+            
+            frames.frames2data()
+            
+            proj.remove_data([-4,-3,-1],delete=True)
+            proj[-1].source.additional_info=f'chunk{k}'
+            proj.update_info()
+            proj[-1].detect.r_no_opt(5)
+            proj[-1].fit()
+            proj.clear_memory()
         
-        frames.frames2data()
+        appendDataObjs(proj[-5:])    
+        proj.remove_data([-6,-5,-4,-3,-2],delete=True)
+        proj[-1].source.additional_info=None
         
-        proj.remove_data([-4,-3,-1]) 
-        proj[-1].detect.r_no_opt(15)
-        proj[-1].fit()
-        proj.save()
+            
