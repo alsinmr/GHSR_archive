9f43309c7c7fee66c51cf35953f72d550b46595f
Andy <Albert.Smith-Penzel@medizin.uni-leipzig.de>
2022-11-21 16:15:44 +0100

    Update setup_iRED_ghrelin1.py

--- setup_iRED_ghrelin1.py
+++ setup_iRED_ghrelin1.py
@@ -12,21 +12,7 @@ import pyDR
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-
-def load_helices():
-    name=list()
-    resids=list()
-    with open('helix_assign.txt','r') as f:
-        for line in f:
-            name.append(line.strip().split(':')[0])
-            ar=np.arange(\
-                int(line.split('resid')[1].split('to')[0].strip()),
-                int(line.split('to')[1].strip())+1)
-            if len(resids):
-                ar=ar[np.logical_not(np.isin(ar,np.concatenate(resids)))]
-            resids.append(ar)
-    return name,resids
-
+from misc_functions import load_helices
 
 #%% File locations
 proj=pyDR.Project('Projects/ghrelin_iRED_half',create=True)
@@ -43,7 +29,7 @@ trajs=[f'WT-ghrelin_run{k}_0.1ns_just_protein.xtc' for k in range(1,4)]
 for traj in trajs:
     for t0,tf in zip([0,177500],[177500,355000]):
         molsys=pyDR.MolSys(topo=os.path.join(mddir,topo),
-                           traj_files=os.path.join(mddir,traj),step=1,
+                           traj_files=os.path.join(mddir,traj),step=100,
                            t0=t0,tf=tf,project=proj)
         
         sel=pyDR.MolSelect(molsys)
