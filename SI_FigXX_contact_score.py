f33000c5a67535c9d7161daabe115796900ec64c
Andy <Albert.Smith-Penzel@medizin.uni-leipzig.de>
2023-05-02 13:13:41 +0200

    Updates (contact score)

--- SI_FigXX_contact_score.py
+++ SI_FigXX_contact_score.py
@@ -6,11 +6,26 @@ Created on Tue May  2 12:49:04 2023
 @author: albertsmith
 """
 
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+with open(os.path.join('contact_score','R.data'),'rb') as f:
+    R=np.load(f,allow_pickle=False)
+with open(os.path.join('contact_score','r0.data'),'rb') as f:
+    r0=np.load(f,allow_pickle=False)
+
 
 
 #%% Calculate Q
-beta=5
-Lambda=1.
-Q=(1/(1+np.exp(beta*(R-Lambda*r0)))).mean(1)
-ax=plt.figure().add_subplot(111)
-ax.plot(Q)
+beta=2
+Lambda=1
+Q=(1/(1+np.exp(beta*(R-Lambda*r0))))
+
+i=np.std(Q,axis=0)>.3
+Q=Q.T[i].mean(0)
+
+fig,ax=plt.subplots(3,1)
+for k,a in enumerate(ax):
+    a.plot(Q[len(Q)//3*k:len(Q)//3*(k+1)])
